40e4509da92b1da3efe0a5c5aedcb33d1d151957
edge <dev@ionized.io>
2015-10-11 19:20:37 -0400

    clean up code

--- muramura/src/PlatformManager.js
+++ muramura/src/PlatformManager.js
@@ -8,20 +8,17 @@
 
 //============================================================
 //============================================================
-//  class PlatformManager     extend IPlatformManager
+//  class PlatformManager	 extend IPlatformManager
 //============================================================
 //============================================================
-function PlatformManager()
-{
-
-}
+function PlatformManager() {}
 
 //============================================================
-//    PlatformManager # loadBytes()
+//	PlatformManager # loadBytes()
 //============================================================
-PlatformManager.prototype.loadBytes       = function(path/*String*/, callback)
+PlatformManager.prototype.loadBytes	   = function(path/*String*/, callback)
 {
-    var request = new XMLHttpRequest();
+	var request = new XMLHttpRequest();
 	request.open("GET", chrome.extension.getURL(path), true);
 	request.responseType = "arraybuffer";
 	request.onload = function(){
@@ -39,105 +36,107 @@ PlatformManager.prototype.loadBytes       = function(path/*String*/, callback)
 }
 
 //============================================================
-//    PlatformManager # loadString()
+//	PlatformManager # loadString()
 //============================================================
-PlatformManager.prototype.loadString      = function(path/*String*/)
+PlatformManager.prototype.loadString	  = function(path/*String*/)
 {
-    
-    this.loadBytes(path, function(buf) {        
-        return buf;
-    });
-    
-}
 
-//============================================================
-//    PlatformManager # loadLive2DModel()
-//============================================================
-PlatformManager.prototype.loadLive2DModel = function(path/*String*/, callback)
-{
-    var model = null;
-    
-    // load moc
-	this.loadBytes(path, function(buf){
-		model = Live2DModelWebGL.loadModel(buf);
-        callback(model);
+	this.loadBytes(path, function(buf) {
+		return buf;
 	});
 
 }
 
 //============================================================
-//    PlatformManager # loadTexture()
+//	PlatformManager # loadLive2DModel()
 //============================================================
-PlatformManager.prototype.loadTexture     = function(model/*ALive2DModel*/, no/*int*/, path/*String*/, callback)
-{ 
-    // load textures
-    var loadedImage = new Image();
-    loadedImage.src = chrome.extension.getURL(path);
-    loadedImage.crossOrigin = "Anonymous";
-        
-    var thisRef = this;
-    loadedImage.onload = function() {
-                
-        // create texture
-        var canvas = document.getElementById("glcanvas");
-        var gl = getWebGLContext(canvas, {premultipliedAlpha : true});
-        var texture = gl.createTexture();	 
-        if (!texture){ console.error("Failed to generate gl texture name."); return -1; }
+PlatformManager.prototype.loadLive2DModel = function(path/*String*/, callback)
+{
+	var model = null;
 
-        gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, 1);
-        gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);	
-        gl.activeTexture(gl.TEXTURE0);
-        gl.bindTexture(gl.TEXTURE_2D, texture);
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, 
-                      gl.UNSIGNED_BYTE, loadedImage);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
-        gl.generateMipmap(gl.TEXTURE_2D);
+	// load moc
+	this.loadBytes(path, function(buf){
+		model = Live2DModelWebGL.loadModel(buf);
+		callback(model);
+	});
+
+}
+
+//============================================================
+//	PlatformManager # loadTexture()
+//============================================================
+PlatformManager.prototype.loadTexture	 = function(model/*ALive2DModel*/, no/*int*/, path/*String*/, callback)
+{
+	// load textures
+	var loadedImage = new Image();
+	loadedImage.src = chrome.extension.getURL(path);
+	loadedImage.crossOrigin = '';
+
+	var thisRef = this;
+	loadedImage.onload = function() {
+
+		// create texture
+		var canvas = document.getElementById("glcanvas");
+		var gl = getWebGLContext(canvas, { premultipliedAlpha: true });
+		var texture = gl.createTexture();
+		if (!texture) {
+			console.error("Failed to generate gl texture name.");
+			return -1;
+		}
+
+		gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, 1);
+		gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);
+		gl.activeTexture(gl.TEXTURE0);
+		gl.bindTexture(gl.TEXTURE_2D, texture);
+		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,
+					  gl.UNSIGNED_BYTE, loadedImage);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+		gl.generateMipmap(gl.TEXTURE_2D);
 
 
-        
-        model.setTexture(no, texture);
-        
-        // テクスチャオブジェクトを解放
-        texture = null;
-        
-        if (typeof callback == "function") callback();
-    };
-    
-    loadedImage.onerror = function() { 
-        console.error("Failed to load image : " + path); 
-    }
+
+		model.setTexture(no, texture);
+
+		// テクスチャオブジェクトを解放
+		texture = null;
+
+		if (typeof callback == "function") callback();
+	};
+
+	loadedImage.onerror = function() {
+		console.error("Failed to load image : " + path);
+	}
 }
 
 
 //============================================================
-//    PlatformManager # parseFromBytes(buf)
+//	PlatformManager # parseFromBytes(buf)
 
 //============================================================
 PlatformManager.prototype.jsonParseFromBytes = function(buf){
-    
-    var jsonStr;
-    
-    
-    
-    var bomCode = new Uint8Array(buf, 0, 3);
-    if (bomCode[0] == 239 && bomCode[1] == 187 && bomCode[2] == 191) {
-        jsonStr = String.fromCharCode.apply(null, new Uint8Array(buf, 3));
-    } else {
-        jsonStr = String.fromCharCode.apply(null, new Uint8Array(buf));
-    }
-    
-    var jsonObj = JSON.parse(jsonStr);
-    
-    return jsonObj;
+
+	var jsonStr;
+
+
+
+	var bomCode = new Uint8Array(buf, 0, 3);
+	if (bomCode[0] == 239 && bomCode[1] == 187 && bomCode[2] == 191) {
+		jsonStr = String.fromCharCode.apply(null, new Uint8Array(buf, 3));
+	} else {
+		jsonStr = String.fromCharCode.apply(null, new Uint8Array(buf));
+	}
+
+	var jsonObj = JSON.parse(jsonStr);
+
+	return jsonObj;
 };
 
 
 //============================================================
-//    PlatformManager # log()
+//	PlatformManager # log()
 //============================================================
-PlatformManager.prototype.log             = function(txt/*String*/)
+PlatformManager.prototype.log			 = function(txt/*String*/)
 {
-    console.log(txt);
+	console.log(txt);
 }
-
